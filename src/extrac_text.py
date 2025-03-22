09e0ffd808c1c4da65c82ee34ddcd511d499f7a7
PedroLopes <Pedro.Lopesverardo@gmail.com>
2025-03-21 21:35:58 -0300

    bold section extraction

--- src/extrac_text.py
+++ src/extrac_text.py
@@ -1,40 +1,45 @@
 import fitz
 import re
 import logging
-from src.regex_pattern import RegexPattern
-import time
+from regex_pattern import RegexPattern
 
 logger = logging.getLogger(__name__)
 
-# def extract_text_with_metadata(pdf_path):
-#     doc = fitz.open(pdf_path)
-#     extracted_text = []
+def extract_text_with_metadata(pdf_path, section_pattern):
+    doc = fitz.open(pdf_path)
+    extracted_text = []
     
-#     result = []
-#     patterns = RegexPattern()
+    result = []
+    patterns = RegexPattern()
+    non_ascii = patterns.get_pattern("non_ascii")
+    unicode_spaces = patterns.get_pattern("unicode_spaces")
+    number_text = patterns.get_pattern(section_pattern)
 
-#     for page_number, page in enumerate(doc, start=1):
-#         blocks = page.get_text("dict")["blocks"]
-#         for block in blocks:
-#             if "lines" in block:
-#                 for line in block["lines"]:
-#                     for span in line["spans"]:
-#                         text = span["text"].strip()
-#                         non_ascii = patterns.get_pattern("non_ascii")
-#                         clean_text = re.sub(non_ascii, "", text)
-#                         unicode_spaces = patterns.get_pattern("unicode_spaces")
-#                         clean_text = re.sub(unicode_spaces, " ", clean_text)
-#                         font = span["font"]
-#                         if "bold" in font.lower():
-#                             result.append((clean_text))
-#                         size = int(span["size"])
-                        
-#                         if text: 
-#                             extracted_text.append(f"{text} <font:{font}> <size:{size}>")
+
+    for _, page in enumerate(doc, start=1):
+        blocks = page.get_text("dict")["blocks"]
+        for block in blocks:
+            if "lines" in block:
+                for line in block["lines"]:
+
+                    for span in line["spans"]:
+                        if "bold" in span["font"].lower():
+                            bold = True
+                        else:
+                            bold = False
+                    
+                    if bold:
+
+                        line_text = " ".join(span["text"] for span in line["spans"])
+                        clean_text = re.sub(non_ascii, "", line_text)
+                        clean_text = re.sub(unicode_spaces, " ", clean_text)
+
+                        if re.search(number_text, clean_text):
+                            result.append((clean_text))
     
-#     return result
+    return result 
 
-def extract_text(pdf_path: str) -> str:
+def extract_text(pdf_path):
     doc = fitz.open(pdf_path)
     text = ''
     for page in doc:
@@ -42,71 +47,55 @@ def extract_text(pdf_path: str) -> str:
     doc.close()
     return text
 
-def extract_sections(pdf_text: str, section_pattern: str):
+def ExtractText(pdf_text: str, section_pattern: str):
 
+
+    # Limpa o texto extraído
     patterns = RegexPattern()
     non_ascii = patterns.get_pattern("non_ascii")
-    if not isinstance(pdf_text, str):
-        pdf_text = str(pdf_text)
-
     clean_text = re.sub(non_ascii, "", pdf_text)
-    logging.info("Time to clean text: %s", time.time())
+
+    with open("output.txt", "w") as f:
+            f.write(clean_text)
 
     # "pattern": "\\n\\s*[Rr][Ee][Ff][Ee][Rr][Ee][Nn][Cc][Ee][Ss]\\s*\\n",
     reference_pattern = patterns.get_pattern("references")
-    split_text = re.split(reference_pattern, clean_text, flags=re.IGNORECASE, maxsplit=1)
-    logging.info("Time to split reference: %s", time.time())
+    clean_text, reference = re.split(reference_pattern, clean_text, flags=re.IGNORECASE, maxsplit=1)
 
-    if len(split_text) == 2:
-        clean_text, reference = split_text
-    else:
-        clean_text, reference = split_text[0], ""
+    table_description_pattern = patterns.get_pattern("table_description")
+    clean_text = re.sub(table_description_pattern, "", clean_text)
 
+    reference_dict = {}
+    citation_pattern = patterns.get_pattern("citation1")
 
-    # table_description_pattern = patterns.get_pattern("table_description")
-    # clean_text = re.sub(table_description_pattern, "", clean_text)
-
-    #TODO: Implement reference extraction
-
-    # reference_dict = {}
-    # citation_pattern = patterns.get_pattern("citation1")
-
-    # matches = re.findall(citation_pattern, reference, re.DOTALL)
-    # for match in matches:
-    #     reference_dict[int(match[0])] = match[1].strip()
+    matches = re.findall(citation_pattern, reference, re.DOTALL)
+    for match in matches:
+        reference_dict[int(match[0])] = match[1].strip()
 
     document_section_pattern = patterns.get_pattern(section_pattern)
     matches = list(re.finditer(document_section_pattern, clean_text, re.MULTILINE))
-    logging.info("Time to split sections: %s", time.time())
 
+    with open("output.txt", "w") as f:
+            f.write(clean_text)
+
+    # Segmenta o texto
     segments = []
     for i in range(len(matches)):
         start = matches[i].end()
         end = matches[i + 1].start() if i + 1 < len(matches) else len(clean_text)
         segment_text = clean_text[start:end].strip()
         segments.append((matches[i].group().strip(), segment_text))
-    logging.info("Time to extract segments: %s", time.time())
 
-    logger.info("DOCUMENT SECTION DEBUG:")
+    logger.info("Document sections:")
     for i in segments:
         logger.info(re.sub(r"\n", " ", i[0]))
 
-    return segments
-
 
 if __name__ == "__main__":
-    pdf_path = "jailmaReviewPdf/jamshidi2017b.pdf"
-    extracted_text = extract_text(pdf_path)
+    pdf_path = "pdfs/ANALYSISONTHESTRATEGYANDIMPLEMENTATIONOFDIGITALTECHNOLOGYINTHEXYZCOMPANY.pdf"
+    extracted_text = extract_text_with_metadata(pdf_path, "numeric_point_section")
 
-    start_time = time.time()
-    segments = extract_sections(extracted_text, "rome_point_section")
-    end_time = time.time()
-    logger.info("ExtractText function took %s seconds", end_time - start_time)
+    print(extracted_text)
 
-    # with open("output.json", "w") as f:
-    #     json.dump(segments, f, indent=4)
-
-
-    
-    
-    
+    # "pattern": "(?<=\\n)(\\d+)\\.\\s*[A-Z][^\\n]+\\n"
+    # ExtractText(extracted_text, "numeric_section")
